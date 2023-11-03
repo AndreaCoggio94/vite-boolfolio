8fe8e501135650d4b0ae78a537e0bf2683c21a78
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-11-03 18:16:39 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import ProjectList from "./components/projects/ProjectList.vue";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      projects: [],
+      api: {
+        baseUrl: "http://localhost:8000/api/",
+      },
+    };
+  },
+  components: { ProjectList },
+
+  methods: {
+    fetchProjects(uri = this.api.baseUrl + "projects") {
+      axios.get(uri).then((response) => {
+        this.projects = response.data.data;
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
+};
+</script>
+
+<template>
+  <h1>Hello There!</h1>
+  <ProjectList></ProjectList>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,15 @@
+<script>
+// import ProjectList from "./components/projects/ProjectList.vue";
+export default {
+  data() {
+    return {};
+  },
+  //   components: { ProjectList },
+};
+</script>
+
+<template>
+  <h2>Project list</h2>
+</template>
+
+<style lang="scss" scoped></style>
