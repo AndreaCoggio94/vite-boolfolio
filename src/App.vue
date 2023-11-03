ceb2e9efc5227c6a08089b089bb7ed2d4a69c1f4
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-11-03 18:39:41 +0100

    added store and moved axios fetch to projectlist

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,17 @@
 <script>
 import ProjectList from "./components/projects/ProjectList.vue";
-import axios from "axios";
 
 export default {
   data() {
-    return {
-      projects: [],
-      api: {
-        baseUrl: "http://localhost:8000/api/",
-      },
-    };
+    return {};
   },
   components: { ProjectList },
-
-  methods: {
-    fetchProjects(uri = this.api.baseUrl + "projects") {
-      axios.get(uri).then((response) => {
-        this.projects = response.data.data;
-      });
-    },
-  },
-  created() {
-    this.fetchProjects();
-  },
 };
 </script>
 
 <template>
   <h1>Hello There!</h1>
-  <ProjectList :projects="projects"></ProjectList>
+  <ProjectList></ProjectList>
 </template>
 
 <style lang="scss"></style>
--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -1,13 +1,27 @@
 <script>
 import ProjectCard from "./ProjectCard.vue";
+import axios from "axios";
+
+import { store } from "../../data/store";
+
 export default {
   data() {
-    return {};
+    return {
+      projects: [],
+    };
   },
-  props: {
-    projects: Array,
-  },
+  props: {},
   components: { ProjectCard },
+  methods: {
+    fetchProjects(uri = store.api.baseUrl + "projects") {
+      axios.get(uri).then((response) => {
+        this.projects = response.data.data;
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
 };
 </script>
 
