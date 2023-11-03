796170322bd25da668da36fbf5899136e7ee8a27
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-11-03 19:09:26 +0100

    added labels in projectCard

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -12,8 +12,30 @@ export default {
 <template>
   <div class="col">
     <div class="card">
+      <div
+        class="card-header d-flex justify-content-between align-content-start"
+      >
+        <span
+          v-if="project.type"
+          class="badge mx-1"
+          :style="{ backgroundColor: project.type.colour }"
+          >{{ project.type.name }}</span
+        >
+
+        <div class="div">
+          <span
+            class="badge mx-1 rounded-pill"
+            v-for="technology in project.technologies"
+            :style="{ backgroundColor: technology.colour }"
+            >{{ technology.label }}</span
+          >
+        </div>
+      </div>
       <div class="card-body">
         <h4>{{ project.name }}</h4>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">More</a>
       </div>
     </div>
   </div>
